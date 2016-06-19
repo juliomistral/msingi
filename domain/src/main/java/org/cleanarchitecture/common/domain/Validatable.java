66f89e2189ebaf507c0c2d4598b67e64b8ad8803
Julio A. Mistral <julio.mistral@gmail.com>
2016-06-18 18:10:23 -0700

    Some cleanup of Validatable and Repository

--- domain/src/main/java/org/cleanarchitecture/common/domain/Validatable.java
+++ domain/src/main/java/org/cleanarchitecture/common/domain/Validatable.java
@@ -3,6 +3,5 @@ package org.cleanarchitecture.common.domain;
 import javax.validation.ConstraintViolationException;
 
 public interface Validatable {
-
     void validate() throws ConstraintViolationException;
 }
--- domain/src/main/java/org/cleanarchitecture/common/repo/Repository.java
+++
@@ -1,9 +0,0 @@
-package org.cleanarchitecture.common.repo;
-
-import org.cleanarchitecture.common.domain.Entity;
-import org.cleanarchitecture.common.domain.Id;
-
-public interface Repository<E extends Entity<E>> {
-    E get(Id<E> id);
-    Id<E> create(E entity);
-}
